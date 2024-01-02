2c853cb6870bb3f8ff4c147537c563b182a59287
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2024-01-02 16:12:35 +0300

    feat: clear project, add options API and composition API

---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <h2>{{ count }}</h2>
+  <button @click="increment">Click me</button>
+</template>
