e48dab06b22e6ff899b7c57ec63f3ef5851f78cc
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2024-01-04 11:51:01 +0300

    feat: add images and icons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
-import CardItem from './components/CardItem.vue';
 </script>
 
 <template>
-  <CardItem title="Кроссовки Puma" :price="9500" is-added>
-    <button class="text-gray-500">Добавить</button>
-  </CardItem>
-  <CardItem title="Кроссовки Nike" :price="11500" :is-added="false"/>
-  <CardItem title="Кроссовки Kike" :price="1500" :is-added="false"/>
+  <h1>It's begin</h1>
 </template>
 
 <style scoped>
