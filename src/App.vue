9f50b68f1ed3363cb9933c8ec919f9017305f4e7
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2024-01-08 21:40:54 +0300

    feat: add Card component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
 import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
-  <div class="w-4/5 h-screen m-auto rounded-xl bg-white shadow-xl mt-14">
+  <div class="w-4/5 m-auto rounded-xl bg-white shadow-xl mt-14 mb-14">
     <Header />
+
+    <div class="flex flex-wrap p-10 gap-10">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   </div>
 </template>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="relative border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img src="/like-1.svg" alt="like" class="absolute top-8 left-8" />
+    <img src="/sneakers/sneakers-1.jpg" alt="" />
+    <p class="mt-2">Мужские кроссовки Nike Blazer Mid Suede</p>
+
+    <div class="flex justify-between mt-2">
+      <div class="flex flex-col">
+        <span class="text-slate-00">Цена:</span>
+        <b>1205 руб.</b>
+      </div>
+      <img src="/plus.svg" alt="Plus" />
+    </div>
+  </div>
+</template>
