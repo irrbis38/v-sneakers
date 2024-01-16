d7c09ebb49e5a8fbbfd920a58b9c5c0bb265180f
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2024-01-16 17:57:13 +0300

    feat: add search and sort fields

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,26 @@ import CardList from './components/CardList.vue'
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевые)</option>
+            <option value="">По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute top-3 left-4" src="/search.svg" alt="" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList />
     </div>
